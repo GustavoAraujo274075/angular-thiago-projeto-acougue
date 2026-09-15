fb4ce425c0f70da479038fc263e2bf09aa35e80b
GustavoAraujo274075 <274075@unifio.edu.br>
2026-09-15 01:14:09 +0000

    Ex 5 Lista de EX

---
+++ primeiro/src/app/lista-ex/ex6/ex6.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ex6',
+  standalone: false,
+  templateUrl: './ex6.html',
+  styleUrl: './ex6.scss',
+})
+export class Ex6 {
+  contagem: number = 0;
+
+  aumentarContagem(){
+    this.contagem++
+  }
+  diminuirContagem(){
+    this.contagem--
+  }
+}
