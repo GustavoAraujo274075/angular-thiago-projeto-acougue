f12e3d159c1270d0f6e66420c89890b143f68ddc
GustavoAraujo274075 <274075@unifio.edu.br>
2026-09-15 01:03:56 +0000

    Ex 4 Lista de EX

---
+++ primeiro/src/app/lista-ex/ex4/ex4.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ex4',
+  standalone: false,
+  templateUrl: './ex4.html',
+  styleUrl: './ex4.scss',
+})
+export class Ex4 {
+  formularioValido: boolean = false;
+
+  alternarValidade(): void {
+    this.formularioValido = !this.formularioValido;
+  }
+}
