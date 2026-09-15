2d88384fbf4678b48aff0545cd9bba23ce92ed79
GustavoAraujo274075 <274075@unifio.edu.br>
2026-09-15 00:54:11 +0000

    Ex 3 Lista de EX

---
+++ primeiro/src/app/lista-ex/ex3/ex3.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ex3',
+  standalone: false,
+  templateUrl: './ex3.html',
+  styleUrl: './ex3.scss',
+})
+export class Ex3 {
+imgURL = "https://donafazbem.com.br/wp-content/uploads/2024/04/acougue-brasilia-dona.jpg";
+}
--- primeiro/src/app/lista-ex/lista-ex-module.ts
+++ primeiro/src/app/lista-ex/lista-ex-module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { ListaExRoutingModule } from './lista-ex-routing-module';
 import { Ex1 } from './ex1/ex1';
 import { Ex2 } from './ex2/ex2';
+import { Ex3 } from './ex3/ex3';
 
 @NgModule({
-  declarations: [Ex1, Ex2],
+  declarations: [Ex1, Ex2, Ex3],
   imports: [CommonModule, ListaExRoutingModule],
 })
 export class ListaExModule {}
