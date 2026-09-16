67ab55f2e8714410c63c5128cc37eb04c4f8876e
GustavoAraujo274075 <274075@unifio.edu.br>
2026-09-16 00:17:55 +0000

    Ex 9 Lista de EX

--- primeiro/src/app/lista-ex/ex9/ex9.ts
+++ primeiro/src/app/lista-ex/ex9/ex9.ts
@@ -6,4 +6,16 @@ import { Component } from '@angular/core';
   templateUrl: './ex9.html',
   styleUrl: './ex9.scss',
 })
-export class Ex9 {}
+export class Ex9 {
+  estoque: number = 0;
+  item = '';
+
+  aumentarEstoque(){
+    this.estoque++
+  }
+  diminuirEstoque(){
+    if (this.estoque > 0){
+      this.estoque--
+    }    
+  }
+}
