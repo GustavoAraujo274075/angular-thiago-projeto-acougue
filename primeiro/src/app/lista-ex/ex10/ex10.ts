fa4288c9463c98fa5933ad0b90b185cb5dad2c01
GustavoAraujo274075 <274075@unifio.edu.br>
2026-09-16 00:35:26 +0000

    Ex 10 Lista de EX

--- primeiro/src/app/lista-ex/ex10/ex10.ts
+++ primeiro/src/app/lista-ex/ex10/ex10.ts
@@ -6,4 +6,20 @@ import { Component } from '@angular/core';
   templateUrl: './ex10.html',
   styleUrl: './ex10.scss',
 })
-export class Ex10 {}
+export class Ex10 {
+user = '';
+senha = '';
+userLogin = '';
+senhaLogin = '';
+cadastrado: boolean = false;
+
+checarLogin() {
+  if (this.userLogin === this.user && this.senhaLogin === this.senha) {
+    alert('Login realizado com sucesso!');
+  } else {
+    alert('Usuário ou senha incorretos.');
+  }
+}
+
+
+}
