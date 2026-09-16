cb17fa8f085de893de603e608fbbfb3bff881d00
GustavoAraujo274075 <274075@unifio.edu.br>
2026-09-16 00:03:37 +0000

    Ex 8 Lista de EX

--- primeiro/src/app/lista-ex/ex7/ex7.ts
+++ primeiro/src/app/lista-ex/ex7/ex7.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms'; 
 
 @Component({
   selector: 'app-ex7',
-  imports: [FormsModule], 
+  standalone: false,
   templateUrl: './ex7.html',
   styleUrl: './ex7.scss',
 })
 export class Ex7 {
-  nome = " ";
+  nome = '';
 }
---
+++ primeiro/src/app/lista-ex/ex8/ex8.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ex8',
+  standalone: false,
+  templateUrl: './ex8.html',
+  styleUrl: './ex8.scss',
+})
+export class Ex8 {
+  item = '';
+  quantidade = 0;
+  preco = 0;
+}
