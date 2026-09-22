4db8731f08fb2d40c94f45f10febbd2f7cdd23a0
GustavoAraujo274075 <274075@unifio.edu.br>
2026-09-22 00:58:02 +0000

    Ex 2 Lista de Ex 2

--- primeiro/src/app/lista-ex-2/ex2/ex2.ts
+++ primeiro/src/app/lista-ex-2/ex2/ex2.ts
@@ -6,4 +6,20 @@ import { Component } from '@angular/core';
   templateUrl: './ex2.html',
   styleUrl: './ex2.scss',
 })
-export class Ex2 {}
+export class Ex2 {
+
+  usuarioLogado: boolean = false; 
+  mensagem = "Usuário autenticado!";
+  textoBotao = "Entrar";
+
+  alterarTextBotao() {
+    this.usuarioLogado = !this.usuarioLogado;
+
+    if (this.usuarioLogado) {
+      this.textoBotao = "Sair";
+    } else {
+      this.textoBotao = "Entrar";
+    }
+  }
+}
+
