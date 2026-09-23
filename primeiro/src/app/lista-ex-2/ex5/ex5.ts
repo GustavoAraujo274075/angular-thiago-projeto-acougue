affe4c2ffa0cf65f8426aaec35e82a076f823524
GustavoAraujo274075 <274075@unifio.edu.br>
2026-09-23 00:51:20 +0000

    Ex 5 Lista de Ex 2

--- primeiro/src/app/lista-ex-2/ex5/ex5.ts
+++ primeiro/src/app/lista-ex-2/ex5/ex5.ts
@@ -6,4 +6,17 @@ import { Component } from '@angular/core';
   templateUrl: './ex5.html',
   styleUrl: './ex5.scss',
 })
-export class Ex5 {}
+export class Ex5 {
+
+  mostrarProdutos:boolean = true;
+
+  pessoas = [
+    { id: 1, nome: 'Carlos'},
+    { id: 2, nome: 'Gustavo'}, 
+    { id: 3, nome: 'Gabrielly'}]
+
+  alternarProdutos(){
+    this.mostrarProdutos = !this.mostrarProdutos;
+  }
+
+}
