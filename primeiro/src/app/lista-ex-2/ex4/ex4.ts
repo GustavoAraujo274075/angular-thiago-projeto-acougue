3d10f6efbc4b8ed3330e0e54e7c1298fcfdf9918
GustavoAraujo274075 <274075@unifio.edu.br>
2026-09-23 00:27:31 +0000

    Ex 4 Lista de Ex 2

--- primeiro/src/app/lista-ex-2/ex4/ex4.ts
+++ primeiro/src/app/lista-ex-2/ex4/ex4.ts
@@ -1,9 +1,22 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-ex4',
   standalone: false,
   templateUrl: './ex4.html',
   styleUrl: './ex4.scss',
 })
-export class Ex4 {}
+export class Ex4 {
+
+  nomeProduto = 'Teclado';
+  quantidadeEstoque = 5;
+
+  aumentarQuantidade(){
+    this.quantidadeEstoque++
+  }
+  diminuirQuantidade(){
+    if(this.quantidadeEstoque > 0){
+      this.quantidadeEstoque--
+    }
+  }
+
+}
