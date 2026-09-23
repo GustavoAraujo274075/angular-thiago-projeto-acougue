9f3019ee46906af430334b69ab4e309098cd7166
GustavoAraujo274075 <274075@unifio.edu.br>
2026-09-23 02:13:58 +0000

    Ex 7 Lista de Ex 2

--- primeiro/src/app/lista-ex-2/ex7/ex7.ts
+++ primeiro/src/app/lista-ex-2/ex7/ex7.ts
@@ -6,4 +6,18 @@ import { Component } from '@angular/core';
   templateUrl: './ex7.html',
   styleUrl: './ex7.scss',
 })
-export class Ex7 {}
+export class Ex7 {
+
+  disciplinas = [
+    {nome:"Matemática"},
+    {nome:"Português"},
+    {nome:"Geografia"},
+    {nome:"Inglês"},
+    {nome:"Espanhol"},
+    {nome:"Química"}
+  ]
+
+  
+
+
+}
